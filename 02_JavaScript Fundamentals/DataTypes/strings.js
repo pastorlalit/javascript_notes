b9c0928bb8fab709ca601028d1415f0ef293318b
Lalit Pastor <lalit.pastor@ibm.com>
2023-08-23 10:43:17 +0530

    strings and its imp methods

---
+++ 02_JavaScript Fundamentals/DataTypes/strings.js
@@ -0,0 +1,33 @@
+// IndexOf Method 
+
+// The indexOf() method returns the position of the first occurrence of a value in a string.
+
+// The indexOf() method returns -1 if the value is not found.
+
+// The indexOf() method is case sensitive.
+
+let firstName = "Hello"
+let lastName = "Coders"
+
+console.log(lastName.indexOf("e"))
+
+
+// Trim Method -- removed unnecessary spaces from string
+let hobbies = " running  coding  reading ";
+console.log(hobbies);
+let result = hobbies.trim();
+// console.log(result);
+
+
+// lastIndexOf method -- retrun last occurance of string
+console.log(result.lastIndexOf("Running"));
+
+// Split Method - split string based on given spiliter 
+
+let favColors = "Red Yellow Green Orange Blue Pink";
+let arrColors = favColors.split(" ");
+// console.log(arrColors) 
+
+
+
+
